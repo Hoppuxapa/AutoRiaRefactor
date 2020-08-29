3e563e3eda9e01ecc0692334241e2c9d343f847b
Ruslan <hoppuxapa@gmail.com>
2020-08-29 22:55:40 +0300

    Change DDL.sql

--- src/main/resources/DDL.sql
+++ src/main/resources/DDL.sql
@@ -1,15 +1,16 @@
-
 drop type if exists user_role;
-create type user_role as enum('USER', 'ADMINISTRATOR');
+create type user_role as enum ('USER', 'ADMINISTRATOR');
 
 
 drop table if exists users cascade;
-create table users(
-    id serial primary key,
-    email varchar(255),
-    username varchar (255),
-    password varchar(255),
+create table users
+(
+    id         serial primary key,
+    email      varchar(255),
+    age        int Check (age > 0),
+    username   varchar(255),
+    password   varchar(255),
     first_name varchar(255),
-    last_name varchar(255),
-    user_role user_role
+    last_name  varchar(255),
+    user_role  user_role
 );
