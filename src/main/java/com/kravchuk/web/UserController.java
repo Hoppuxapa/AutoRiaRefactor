9bea8d779644a906e9e40e4747c906aa171b08b0
Ruslan <hoppuxapa@gmail.com>
2020-09-24 23:25:55 +0300

    Add list of registered users

--- src/main/java/com/kravchuk/web/UserController.java
+++ src/main/java/com/kravchuk/web/UserController.java
@@ -3,13 +3,14 @@ package com.kravchuk.web;
 import com.kravchuk.domain.User;
 import com.kravchuk.repository.UserRepository;
 import com.kravchuk.repository.UserService;
+import com.kravchuk.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("user")
+@RequestMapping("users")
 public class UserController {
 
     UserRepository userRepository;
@@ -21,20 +22,25 @@ public class UserController {
         this.userService = userService;
     }
 
-
-    @GetMapping("{user}")
-    public String userEdit(@PathVariable User user, Model model) {
-
-        model.addAttribute("user", user);
+/*
+    @GetMapping("/{id}")
+    public String show(@PathVariable long id, Model model){
+        model.addAttribute("user",userRepository.findById(id));
         return "userEdit";
     }
 
+
+    @GetMapping("{user}")
+    public String userEdit(@PathVariable User user, Model model) {
+        model.addAttribute("user", user);
+        return "userEdit";
+    }*/
+
     @RequestMapping("/userList")
-    public String getAllUsers(Model model) {
+    public String listUsers(Model model) {
         model.addAttribute("users", userService.getAllUsers());
         return "userList";
     }
-
 /*
     @PostMapping("/users")
     public String addNewUser(@ModelAttribute("user") User user){
